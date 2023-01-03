1e6d84331a6682fd479255b1a04a02dd8d73b5f1
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2023-01-04 00:57:38 +0500

    p

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 npm install
-# npm run build
 
-DIR="/home/ec2-user/nodejs-react-mongo"
-if [ -d "$DIR" ]; then
-    echo "$DIR exists" 
-else
-    echo "Creating $DIR directory"
-    mkdir $DIR
-fi
+# # npm run build
+
+# DIR="/home/ec2-user/nodejs-react-mongo"
+# if [ -d "$DIR" ]; then
+#     echo "$DIR exists" 
+# else
+#     echo "Creating $DIR directory"
+#     mkdir $DIR
+# fi
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+pm2 restart 0
+
+
 # cd /home/ec2-user/nodejs-react-mongo
 # sudo chmod -R 777 /home/ec2-user/nodejs-react-mongo
 # npm install
 # npm run build
 # sudo npm install pm2 -g
 # pm2 restart "node dist/index.js"
-pm2 restart 0
+
