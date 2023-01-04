b2ad43f4809bdad1fcf3dec7b552dff6a9d6deba
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2023-01-04 16:25:20 +0500

    p

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/nodejs-github-codebuild-codepipeline
+cd /home/ec2-user/nodejs-react-mongo
 npm install
-# echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-# echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-# echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-# cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-# echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
-# npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 pm2 restart nodejs-app
-
-# echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-# echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-# pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
